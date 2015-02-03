db327698933934c23bec4cada2c1989dd097677f
lirao <rao.li@gmail.com>
2015-02-03 14:32:14 -0800

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/Grid.m
+++ GameOfLife.spritebuilder/Source/Grid.m
@@ -148,6 +148,8 @@ static const int GRID_COLUMNS = 10;
 
 - (void) updateCreatures
 {
+	int numAlive = 0;
+
 	// iterate through the rows
 	// note that NSArray has a method 'count' that will return the number of elements in the array
 	for (int i = 0; i < [_gridArray count]; i++) {
@@ -165,8 +167,12 @@ static const int GRID_COLUMNS = 10;
 			{
 				currentCreature.isAlive = NO;
 			}
+			
+			if (currentCreature.isAlive)
+				numAlive++;
 		}
 	}
+	_totalAlive = numAlive;
 }
 
 @end
